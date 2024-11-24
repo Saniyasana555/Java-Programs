1a084e3a33a98183b72fbd8ff446bd9ffffa6be6
Saniya <saniyasana687@gmail.com>
2024-11-24 15:32:36 +0530

    Remove all files from the repository

--- A.java
+++
@@ -1,8 +0,0 @@
-package p1;
-public class A
-{
-public void displayA()
-{
-System.out.println("This is package p1");
-}
-}
--- Simple.java
+++
@@ -1,9 +0,0 @@
-import java.util.StringTokenizer;  
-public class Simple{  
- public static void main(String args[]){  
-   StringTokenizer st = new StringTokenizer("my name is Saniya"," ");  
-     while (st.hasMoreTokens()) {  
-         System.out.println(st.nextToken());  
-     }  
-   }  
-}  
